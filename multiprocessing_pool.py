0a17d48a529c6583afad0f2340e47b66a485d25e
harish joya <harshkjoya@gmail.com>
2019-04-04 17:27:04 +0530

    Update multiprocessing_pool.py

--- multiprocessing_pool.py
+++ multiprocessing_pool.py
@@ -7,22 +7,17 @@ def f(n):
         sum += x*x
 
 if __name__ == "__main__":
+
     print("In main")
     pl = mp.Pool()
     t1 = time.time()
     result = pl.map(f,range(100000))
     pl.close()
     pl.join()
-
     print("Pool took this much time: " + str(time.time()-t1))
 
     t2 = time.time()
     result = []
     for x in range(100000):
         result.append(f(range(100000)))
-
     print("Serial took this much time: " + str(time.time() - t2))
-
-
-
-
